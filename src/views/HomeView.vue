ec26757ff2a7c729fa4a30d61e59f97cda69c13d
Gudo Fabi <godofredocpanel@gmail.com>
2022-05-13 21:18:22 +0800

    initial deployment

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,17 +7,22 @@
         <div class="content">
           <h1>Gudo fabi</h1>
           <p>Frontend developer, teacher, and web junkie. I love creating new things, discovering designing trends, and  transforming ideas into reality.</p>
+          <img src="/images/astro-helmet-2.png" alt="" class="md:hidden">
         </div>
       </div>
       <div class="header-img">
-        <img src="/images/astro.png" alt="Gudo Fabi">
+        <img src="/images/astro-lg.png" alt="Header Astro">
+        <!-- <picture>
+          <source media="(min-width: 1024px)" srcset="/images/astro-lg.png">
+          <img src="/images/astro-md.png" alt="Header Astro">
+        </picture> -->
       </div>
     </div>
 
-    <div class="scroll-down mt-10 mb-28">
+    <div class="scroll-down my-20 ">
       <a href="" class="flex flex-col items-center justify-center">
         <p class="mb-3">Keep Scrolling</p>
-        <fa :icon="['fas','arrow-down']" size="2x" />
+        <fa :icon="['fas','arrow-down']" size="2x" class="animate-bounce"/>
       </a>
     </div>
 
@@ -96,29 +101,44 @@ export default {
 
 <style lang="scss" scoped>
 .home {
+
   h3 {
-    @apply font-holiday text-4xl mb-6 leading-[1.9];
+    @apply font-holiday text-[28px] md:text-4xl mb-6 leading-[1.9];
   }
   .header {
-    @apply flex items-end justify-around max-w-[920px] w-full mx-auto;
+    @apply relative flex md:items-end md:justify-around 
+      max-w-[320px] sm:max-w-[570px] md:max-w-[640px] xl:max-w-[920px] w-full mx-auto;
 
     .header-content {
       @apply relative w-[505px] h-[370px] flex flex-col justify-center;
 
       .yellow-brick {
-        @apply absolute w-[222px] h-full left-0 top-0 bg-gf-yellow -z-10;
+        @apply absolute w-[172px] md:w-[161px] xl:w-[222px] h-full left-0 top-0 bg-gf-yellow -z-10;
 
       }
       .content {
-        @apply max-w-[470px] w-full ml-11;
+        @apply max-w-[270px] md:max-w-[470px] w-full ml-11;
 
         h1 {
-          @apply font-holiday text-black text-[96px] w-full mb-8 tracking-tight;
+          @apply font-holiday text-black text-5xl md:text-[62px] xl:text-[96px] w-full mb-8 tracking-tight;
         }
 
         p {
-          @apply leading-7 tracking-widest;
+          @apply leading-6 xl:leading-7 tracking-widest text-sm md:text-base;
         }
+
+        img {
+          @apply block sm:hidden absolute right-0;
+        }
+
+      }
+    }
+
+    .header-img {
+      @apply hidden sm:block;
+
+      img {
+        @apply max-w-[308px] xl:max-w-full;
 
       }
     }
@@ -126,24 +146,30 @@ export default {
   }
 
   .about {
-    @apply relative flex max-w-[900px] mx-auto items-start justify-between mb-36;
+    @apply relative flex flex-col-reverse lg:flex-row 
+      max-w-[320px] md:max-w-[800px] xl:max-w-[900px] mx-auto 
+      items-center xl:items-start justify-center xl:justify-between mb-36;
 
     .about-bg-img {
-      @apply -z-10 bottom-0 left-[13%];
+      @apply w-full md:w-[unset] lg:-z-10 bottom-0 lg:left-[13%];
     }
     .about-balloon-img {
-      @apply right-8 -top-32 -z-10;
+      @apply hidden md:block right-16 lg:right-8 -top-32 lg:-z-10;
     }
 
     .astro-img {
-      @apply w-1/2;
+      @apply w-full md:w-1/2 z-10;
+
+      img {
+        @apply w-[90%];
+      }
 
     }
     .about-content {
-      @apply w-1/2;
+      @apply w-full md:w-[60%] lg:w-1/2;
 
       p {
-        @apply text-black tracking-widest leading-7 mb-4;
+        @apply text-black tracking-widest leading-7 mb-4 text-sm md:text-base;
 
         span {
           @apply text-gf-dark-yellow font-semibold;
@@ -152,11 +178,11 @@ export default {
       }
 
       .qoute-message {
-        @apply text-gray-500 italic pt-4;
+        @apply text-gray-500 italic pt-4 text-sm md:text-base;
       }
 
       .qoute-owner {
-        @apply text-gray-600 font-semibold text-right;
+        @apply text-gray-600 font-semibold text-right text-sm md:text-base;
       }
     }
 
@@ -166,10 +192,11 @@ export default {
     @apply max-w-[900px] mx-auto mb-28;
 
     .services-container {
-      @apply flex justify-between items-center;
-
+      @apply flex justify-center lg:justify-between flex-wrap items-center;
       .card {
-        @apply flex items-center bg-white rounded-xl shadow-lg hover:shadow-2xl border-2 border-transparent hover:border-gf-yellow py-5 px-9 transition-all;
+        @apply flex items-center bg-white rounded-xl shadow-lg 
+          hover:shadow-2xl border-2 border-transparent hover:border-gf-yellow py-5 px-9 transition-all
+          first:mr-8 mb-8 last:m-0 lg:m-0 lg:first:m-0;
 
         .img-cover {
           @apply w-20 h-20 mr-5;
@@ -190,10 +217,11 @@ export default {
     @apply pt-20 pb-16 relative overflow-hidden;
 
     img {
-      @apply absolute max-w-[700px] w-full h-auto mr-[-350px] ml-[-350px] right-1/2 left-1/2 -z-10 top-0 animate-spin-slow;
+      @apply absolute max-w-[700px] md:w-full h-auto 
+        md:mr-[-350px] md:ml-[-350px] md:right-1/2 md:left-1/2 -z-10 top-0 animate-spin-slow;
     }
     .content {
-      @apply  mx-auto max-w-[522px] w-full text-center;
+      @apply  mx-auto max-w-[522px] w-full text-center px-5 md:px-0;
 
       p {
         @apply tracking-widest leading-normal mb-10;
